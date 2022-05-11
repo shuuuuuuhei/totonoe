bb551254d6c404a3c98528f0dab20b1cdbea51a0
shuuuuuuhei <tak178.shu10@gmail.com>
2022-05-12 08:00:49 +0900

    jsからtsに置き換え

--- totonoe-app/src/App.js
+++ totonoe-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var logo_svg_1 = require("./logo.svg");
+require("./App.css");
 function App() {
-  return (
-    <div className="App">
+    return (<div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo_svg_1["default"]} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div>);
 }
-
-export default App;
+exports["default"] = App;
