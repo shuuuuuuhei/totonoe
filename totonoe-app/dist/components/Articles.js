c99a206b61108cfd0c71e67d9574ed20c4ade8a5
shuuuuuuhei <tak178.shu10@gmail.com>
2022-05-22 16:59:41 +0900

    ArticlesListの矢印追加

--- totonoe-app/dist/components/Articles.js
+++ totonoe-app/dist/components/Articles.js
@@ -6,6 +6,7 @@ const react_1 = require("react");
 const ArticlesReducer_1 = require("../reducer/ArticlesReducer");
 const Article_1 = require("./Article");
 require("../style/Articles.css");
+const bs_1 = require("react-icons/bs");
 const initialArticlesState = [{
         id: "",
         title: "",
@@ -23,6 +24,6 @@ const initialArticlesState = [{
     }];
 const ArticlesBox = () => {
     const [articlesStore, articlesDispach] = (0, react_1.useReducer)(ArticlesReducer_1.articlesReducer, initialArticlesState);
-    return ((0, jsx_runtime_1.jsx)(react_1.Fragment, { children: (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "list-wrap" }, { children: (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "articles-box container" }, { children: (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "row justify-content-center" }, { children: articlesStore.map((article, index) => (0, jsx_runtime_1.jsx)(Article_1.ArticleBox, {})) })) })) })) }));
+    return ((0, jsx_runtime_1.jsx)(react_1.Fragment, { children: (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "list-wrap" }, { children: (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "articles-box container-fluid" }, { children: (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "row justify-content-center align-items-center" }, { children: [(0, jsx_runtime_1.jsx)(bs_1.BsChevronDoubleLeft, { className: "col-1", size: 45 }), articlesStore.map((article, index) => (0, jsx_runtime_1.jsx)(Article_1.ArticleBox, {})), (0, jsx_runtime_1.jsx)(bs_1.BsChevronDoubleRight, { className: "col-1", size: 45 })] })) })) })) }));
 };
 exports.ArticlesBox = ArticlesBox;
--- totonoe-app/src/components/Articles.tsx
+++ totonoe-app/src/components/Articles.tsx
@@ -3,6 +3,7 @@ import type { Article } from '../@types/article/Article';
 import { articlesReducer } from '../reducer/ArticlesReducer';
 import { ArticleBox } from './Article';
 import '../style/Articles.css'
+import {BsChevronDoubleLeft, BsChevronDoubleRight} from 'react-icons/bs'
 
 const initialArticlesState: Article[] = [{
     id: "",
@@ -25,9 +26,11 @@ export const ArticlesBox = () => {
     return(
         <Fragment>
             <div className="list-wrap">
-                <div className="articles-box container">
-                    <div className="row justify-content-center">
+                <div className="articles-box container-fluid">
+                    <div className="row justify-content-center align-items-center">
+                        <BsChevronDoubleLeft className="col-1" size={45}/>
                         {articlesStore.map((article, index) => <ArticleBox />)}
+                        <BsChevronDoubleRight className="col-1" size={45}/>
                     </div>
                 </div>
             </div>
