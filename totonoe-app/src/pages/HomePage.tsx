e75c85858b405c8310d3375c31c548880fae1187
shuuuuuuhei <tak178.shu10@gmail.com>
2022-05-23 07:16:35 +0900

    ファイル管理修正

--- totonoe-app/src/pages/HomePage.tsx
+++ totonoe-app/src/pages/HomePage.tsx
@@ -1,10 +1,11 @@
 import React, { Fragment } from "react"
 import Alert from 'react-bootstrap/Alert';
-import { ArticlesBox } from "./Articles"
-import { SearchBox } from "./SearchBox";
+import { ArticlesBox } from "../components/ArticlesBox";
+import { SearchBox } from "../components/SearchBox";
 export const Home: React.VFC = () => {
     return (
         <Fragment>
+            <SearchBox />
             <ArticlesBox />
         </Fragment>
     );
