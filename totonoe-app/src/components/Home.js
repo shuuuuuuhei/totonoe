1d9fa714e1ef4e39ee20305d57ec6e18566b91b0
shuuuuuuhei <tak178.shu10@gmail.com>
2022-05-22 16:14:49 +0900

    jsの生成を~/dist/で行うように修正

--- totonoe-app/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Home = void 0;
-const jsx_runtime_1 = require("react/jsx-runtime");
-const react_1 = require("react");
-const Home = () => {
-    return ((0, jsx_runtime_1.jsx)(react_1.Fragment, { children: (0, jsx_runtime_1.jsx)("div", { className: "" }) }));
-};
-exports.Home = Home;
--- totonoe-app/src/components/form-components/Article.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Article = void 0;
-const Article = () => {
-};
-exports.Article = Article;
