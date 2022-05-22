d0dc8a0fbbd3bc6d5565d094996360f521d1f5b6
shuuuuuuhei <tak178.shu10@gmail.com>
2022-05-22 16:17:40 +0900

    dist 以下にjsファイルを生成

---
+++ totonoe-app/dist/components/Home.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Home = void 0;
+const jsx_runtime_1 = require("react/jsx-runtime");
+const react_1 = require("react");
+const Articles_1 = require("./Articles");
+const Home = () => {
+    return ((0, jsx_runtime_1.jsx)(react_1.Fragment, { children: (0, jsx_runtime_1.jsx)(Articles_1.ArticlesBox, {}) }));
+};
+exports.Home = Home;
---
+++ totonoe-app/dist/components/Proofile.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Profile = void 0;
+const jsx_runtime_1 = require("react/jsx-runtime");
+const react_1 = require("react");
+const Profile = () => {
+    return ((0, jsx_runtime_1.jsx)(react_1.Fragment, { children: (0, jsx_runtime_1.jsx)("h3", { children: "Propfile" }) }));
+};
+exports.Profile = Profile;
