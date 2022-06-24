dfd010c4dbc6e80975bbeabb5dd9af59bdc60aa7
shuuuuuuhei <tak178.shu10@gmail.com>
2022-06-25 08:23:26 +0900

    Auth0導入(クライアント)

--- totonoe-app/src/index.tsx
+++ totonoe-app/src/index.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+import AuthConfig from "./json/auth_config.json"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+        <Auth0Provider 
+            domain={AuthConfig.domain}
+            clientId={AuthConfig.clientId}
+            redirectUri={window.location.origin}
+        >
+        <App />
+        </Auth0Provider>
+        , document.getElementById('root')
+    );
