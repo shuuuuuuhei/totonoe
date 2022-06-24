83b490d8c2b902241f8bd50d3dcd58654a83adbd
shuuuuuuhei <tak178.shu10@gmail.com>
2022-06-25 08:33:40 +0900

    (要検討)DBConfig

---
+++ backend/src/infra/database/config.go
@@ -0,0 +1,34 @@
+package database
+
+//Config DB型宣言
+type Config struct {
+	DB struct {
+		Production struct {
+			Host     string
+			Username string
+			Password string
+			DBName   string
+		}
+		Test struct {
+			Host     string
+			Username string
+			Password string
+			DBName   string
+		}
+	}
+	Routing struct {
+		Port string
+	}
+}
+
+// NewConfig DB接続設定
+func NewConfig() *Config {
+	c := Config{}
+
+	c.DB.Production.Host = "localhost"
+	c.DB.Production.Username = "postgres"
+	c.DB.Production.Password = "postgres"
+	c.DB.Production.DBName = "totonoe"
+	c.Routing.Port = ":8080"
+	return &c
+}
