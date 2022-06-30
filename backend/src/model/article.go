913d3bccacac123d37a57c85a0696484aca7d22d
shuuuuuuhei <tak178.shu10@gmail.com>
2022-07-01 07:23:57 +0900

    Articleモデル型を

--- backend/src/model/article.go
+++ backend/src/model/article.go
@@ -9,11 +9,11 @@ import (
 // Article 記事モデル
 type Article struct {
 	gorm.Model
-	ID          uint32   `gorm:"primryKey;serializer:json"`
-	Title       string `gorm:"primryKey;serializer:json"`
-	Content     string `gorm:"serializer:json"`
-	UserID      string `gorm:"primryKey;serializer:json;foreignKey:UserID"`
-	SaunaID     string `gorm:"primryKey;serializer:json;foreignKey:SaunaID"`
+	ID      uint32 `gorm:"primryKey"`
+	Title   string `gorm:"primryKey"`
+	Content string `gorm:"serializer:json"`
+	UserID  string `gorm:"primryKey;foreignKey:UserID"`
+	// SaunaID     string `gorm:"primryKey;serializer:json;foreignKey:SaunaID"`
 	AricleLikes []ArticleLike
 	Comments    []Comment
 	CreatedAt   time.Time `gorm:"serializer:json;autoCreateTime"`
