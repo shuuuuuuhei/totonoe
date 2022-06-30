50f6748b9ca1ffcbbe8f4d012af5e077b115653c
shuuuuuuhei <tak178.shu10@gmail.com>
2022-07-01 07:44:27 +0900

    Inputコンポーネント

--- totonoe-app/src/components/form-components/Input.tsx
+++ totonoe-app/src/components/form-components/Input.tsx
@@ -2,8 +2,8 @@ type inputProps = {
     type: string,
     name: string,
     value: string,
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
-    placehodlder: string|undefined,
+    onChange: React.ChangeEventHandler,
+    placehodlder?: string,
     errorDiv: string,
     errorMsg: string,
     className: string,
@@ -12,14 +12,16 @@ type inputProps = {
 export const Input = (props: inputProps) => {
     const { type, name, value, onChange, placehodlder, errorDiv, errorMsg, className } = props
     return(
-        <input
-            type={type}
-            className={`form-control ${className}`}
-            id={name}
-            name={name}
-            defaultValue={value}
-            onChange={onChange}
-            placeholder={placehodlder}
-        />
+        <div className="mb-3">
+            <input
+                type={type}
+                className={`form-control ${className}`}
+                id={name}
+                name={name}
+                defaultValue={value}
+                onChange={onChange}
+                placeholder={placehodlder}
+            />
+        </div>
     )
 }
