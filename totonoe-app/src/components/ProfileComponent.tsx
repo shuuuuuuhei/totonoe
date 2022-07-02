89820fa953a20eba5b612979a63d2f0ec106b382
shuuuuuuhei <tak178.shu10@gmail.com>
2022-07-02 16:50:55 +0900

    プロフィール取得fetch処理はProfilePagesで行う

--- totonoe-app/src/components/ProfileComponent.tsx
+++ totonoe-app/src/components/ProfileComponent.tsx
@@ -7,59 +7,10 @@ import { Profile } from '../@types/Profile';
 import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
-export const ProfileComponent = () => {
-    const [profile, setProfile] = useState<Profile>();
-    const {getAccessTokenSilently, user} = useAuth0();
-   
-    useEffect(() => {
-        const fetchProfile = async() => {
-            const uri = "http://localhost:4000/profile";
-            const accessToken = await getAccessTokenSilently({
-                audience: 'https://totonoe-app.com',
-                scope: 'read:posts',
-            });
-
-            if (!accessToken || !user) {
-                throw Error("アクセストークンがありません。");
-            }
-
-            const requestOption: RequestInit = {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    user_id: user.sub?.split('|').at(1)
-                })
-            };
-            await fetch(uri, requestOption)
-            .then((response) => {
-                if (!response.ok) {
-                    const err = new Error;
-                    console.log(response);
-                    err.message = "プロフィールが見つかりませんでした。" + response.status;
-                    throw err;
-                };
-                return response.json();
-            })
-            .then((resData) => {
-                setProfile(resData);
-            })
-            .catch(err => {
-                console.log(err)
-            });
-        }
-        fetchProfile();
-    }, [])
-
-    if(!profile) {
-        return(
-            <Navigate to="/profile" />
-        )
-    }
-
+type profileProps = {
+    profile: Profile|undefined
+}
+export const ProfileComponent: React.VFC<profileProps> = ({profile}) => {
     return (
         <Fragment>
             <h3>Profile</h3>
@@ -71,7 +22,8 @@ export const ProfileComponent = () => {
                         </div>
                         <div className="col-2 user-name">
                             <div className="name">
-                                {profile.nick_name}
+                                name:
+                                {profile?.NickName}
                             </div>
                             <div className="email">
                                 @yamada
@@ -83,8 +35,8 @@ export const ProfileComponent = () => {
                                 <CgDetailsMore />
                             </div>
                             <div className="follow-info row">
-                                <p className="col-6">フォロワー{profile.followerCount}人</p>
-                                <p className="col-6">フォロワー{profile.followingCount}人</p>
+                                <p className="col-6">フォロワー{profile?.followerCount}人</p>
+                                <p className="col-6">フォロワー{profile?.followingCount}人</p>
                             </div>
                         </div>
                     </div>
