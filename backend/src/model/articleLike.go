e7a3bf9a9b79fddf3a47816b2720eb450a008c2b
shuuuuuuhei <tak178.shu10@gmail.com>
2022-07-03 17:57:32 +0900

    ArticleとArticleLike型修正 gormとjsonの併用が可能になった gorm:"->" で読み取り専用のカラムを宣言できる

--- backend/src/model/articleLike.go
+++ backend/src/model/articleLike.go
@@ -9,9 +9,9 @@ import (
 // ArticleLike 記事お気に入りモデル
 type ArticleLike struct {
 	gorm.Model
-	ID        uint32    `gorm:"primryKey;serializer:json"`
-	UserID    string    `gorm:"primryKey;serializer:json;foreignKey:UserID"`
-	ArticleID uint32    `gorm:"primryKey;serializer:json;foreignKey:ArticleID"`
-	CreatedAt time.Time `gorm:"serializer:json;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"serializer:json;autoUpdateTime"`
+	ID        uint32    `gorm:"primryKey"`
+	UserID    string    `gorm:"primryKey;foreignKey:UserID"`
+	ArticleID uint32    `gorm:"primryKey;foreignKey:ArticleID"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
