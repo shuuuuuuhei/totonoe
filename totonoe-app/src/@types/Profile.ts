8548942896a2773a6f71239a3f98f71c726cced2
shuuuuuuhei <tak178.shu10@gmail.com>
2022-07-09 12:27:01 +0900

    プロフィールにフォローフォロワーカウントを追加

--- totonoe-app/src/@types/Profile.ts
+++ totonoe-app/src/@types/Profile.ts
@@ -2,8 +2,9 @@ export type Profile = {
     ID: string
     UserID: string
     NickName: string
-    following_count?: number
-    followed_count?: number
+    following_count: number
+    followed_count: number
     IsMe: boolean
+    IsFollowing: boolean
     Introduction: string
 }
