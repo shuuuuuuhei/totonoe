32c822d779b8dcf4dc03383c5ed3cb3614bea9fe
shuuuuuuhei <tak178.shu10@gmail.com>
2022-08-02 16:22:33 +0900

    CommentオブジェクトにuserNameを追加

--- backend/src/model/ValueObject/comment.go
+++ backend/src/model/ValueObject/comment.go
@@ -6,6 +6,7 @@ import "time"
 type CommentVO struct {
 	ID        uint32    `json:"id,omitempty"`
 	UserID    string    `json:"user_id,omitempty"`
+	UserName  string    `json:"user_name,omitempty"`
 	ArticleID uint32    `json:"article_id,omitempty"`
 	Content   string    `json:"content,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
