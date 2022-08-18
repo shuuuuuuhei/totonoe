863fa68672091ac60d4173cd18cfe68122ae72a8
shuuuuuuhei <tak178.shu10@gmail.com>
2022-08-18 11:19:21 +0900

    ・propsで受け取ったfacilityの情報を表示するよう修正

--- totonoe-app/src/components/SaunaList.tsx
+++ totonoe-app/src/components/SaunaList.tsx
@@ -1,23 +1,36 @@
 import React, { Component, Fragment } from 'react'
 import { HiOutlinePencilAlt } from 'react-icons/hi'
 import { BsHeart } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+import { StrConvertStrTime } from '../common/Convert'
 
-export const SaunaList = () => {
+type FacilityListProps = {
+    facilities: Facility[]
+}
+export const FacilityList = (props: FacilityListProps) => {
     return(
         <Fragment>
-            <div className="facility py-4">
-                <div className="row facility-top">
-                    <div className="col-10 facility-name">
-                        <h3>タイムズ スパ・レスタ </h3>
+            {props.facilities.map((facility) => {
+                return(
+                    <div className="facility py-4">
+                        <div className="row facility-top">
+                            <div className="col-10 facility-name">
+                                <Link to={'/saunas/'+facility.id}><h3>{facility.name}</h3></Link>
+                            </div>
+                            <div className="facility-action col-2 row">
+                                <HiOutlinePencilAlt size={40} className="col-6"/>
+                                <BsHeart size={35} className="col-6"/>
+                            </div>
+                            <p className="facility-address">{facility.address}</p>
+                            <div className="row">
+                                <p className="col-2">入浴料：{facility.price}円</p>
+                                <p className="col-2">営業：{StrConvertStrTime(facility.eigyo_start)}~{StrConvertStrTime(facility.eigyo_end)}</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="facility-action col-2 row">
-                        <HiOutlinePencilAlt size={40} className="col-6"/>
-                        <BsHeart size={35} className="col-6"/>
-                    </div>
-                    <p className="facility-address">東京都 江戸川区</p>
-                    <p>温度：100℃ ととのい度：平均4.6 スペース広い</p>
-                </div>
-            </div>
+                )
+            })}
+
         </Fragment>
     )
 }
