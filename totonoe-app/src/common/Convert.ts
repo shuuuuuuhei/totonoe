00720bf5cb175894242b616af773d081b6936e87
shuuuuuuhei <tak178.shu10@gmail.com>
2022-08-19 22:03:36 +0900

    区分値変換ようの関数を作成

--- totonoe-app/src/common/Convert.ts
+++ totonoe-app/src/common/Convert.ts
@@ -5,5 +5,26 @@ export const UndefinedConvertToZero = (val: number|undefined) => {
     if(!val) {
         return 0;
     }
+
     return val;
 }
+
+/**
+ * 文字列を日付フォーマットに変換する
+ */
+export const StrConvertStrTime = (val: string) => {
+    const hh = val.slice(0, 2)
+    const ss = val.slice(2, 4)
+    return hh+":"+ss
+}
+
+/**
+ * 区分値の変換を行う
+ * 1→"○"
+ * 1以外→"-"
+ */
+export const ConvertKBToMaruORHyphen = (kb: string) => {
+    if(kb != "1") return "-"
+    return "○"
+}
+
