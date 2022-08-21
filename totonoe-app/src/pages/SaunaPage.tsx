966a1e26c6dddcfd6946821668ec964b1d405f50
shuuuuuuhei <tak178.shu10@gmail.com>
2022-08-21 20:53:21 +0900

    サウナ施設に紐づく記事情報取得fetch処理を

--- totonoe-app/src/pages/SaunaPage.tsx
+++ totonoe-app/src/pages/SaunaPage.tsx
@@ -2,10 +2,14 @@ import React, { Component, Fragment, useState, useEffect } from 'react'
 import { SaunaDetail } from '../components/SaunaDetail';
 import { Valuation } from '../components/Valuation';
 import { useParams } from 'react-router-dom';
+import { Article } from '../@types/article/Article';
+import { ArticleList } from '../components/ArticleList';
 
 export const SaunaPage = () => {
 
     const [facility, setFacilityState] = useState<Facility>();
+    const [articles, setArticlesState] = useState<[Article]>();
+
     const [activeMode, setActiveMode] = useState("nav-1");
     const {facilityID} = useParams();
 
@@ -49,7 +53,34 @@ export const SaunaPage = () => {
                 console.log(err)
             });
         }
+        const fetchGetArticlesByFacilityID = async() => {
+            const uri = "http://localhost:4000/facilities/"+facilityID+"/articles";
+            const requestOption: RequestInit = {
+                method: "GET",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+            await fetch(uri, requestOption)
+                .then((response) => {
+                    if (!response.ok) {
+                        const err = new Error;
+                        err.message = "サウナ施設に紐づく記事情報取得に失敗しました" + response.status;
+                        throw err;
+                    }
+                    return response.json();
+                })
+                .then((resData) => {
+                    setArticlesState(resData);
+                    console.log(articles)
+                })
+            .catch(err => {
+                console.log(err)
+            });
+        }
         fetchSauna();
+        fetchGetArticlesByFacilityID();
     }, []);
 
     if(!facility) <Fragment>return("ロード中...")</Fragment>
@@ -59,7 +90,7 @@ export const SaunaPage = () => {
             <div className="container py-5 px-5">
                 <div className="row align-items-center sauna-name py-4">
                     <h1 className="text-center">
-                        タイムズ スパ・レスタ 
+                        {facility?.name}
                     </h1>
                 </div>
                 <ul className="nav nav-pills nav-fill py-3">
@@ -71,7 +102,7 @@ export const SaunaPage = () => {
                     {
                         (() => {
                             if(activeMode == 'nav-1') return( <SaunaDetail facility={facility}/> )
-                            if(activeMode == 'nav-2') return( <>投稿</> )
+                            if(activeMode == 'nav-2') return( !articles ? <>記事情報なし</> : <ArticleList articles={articles} />)
                             if(activeMode == 'nav-3') return( < Valuation/> )
                         })()
                     }
