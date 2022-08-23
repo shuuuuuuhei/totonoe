2dc857ebc2cc0bc4817f317178aef89a6f3656f0
shuuuuuuhei <tak178.shu10@gmail.com>
2022-08-23 22:31:43 +0900

    サウナ施設登録ページを作成

---
+++ totonoe-app/src/pages/SaunaSubmitPage.tsx
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+import { Input } from '../components/form-components/Input'
+import { FacilitySubmitComponent } from '../components/FacilitySubmitComponent'
+
+export const SaunaSubmitPage = () => {
+    const handleChange = () => {
+        
+    }
+
+    return(
+        <Fragment>
+            <FacilitySubmitComponent />
+        </Fragment>
+    )
+}
