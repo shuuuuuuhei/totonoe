c9d65c457aac37ab1ffe415a0e011c03dc7b8ee9
shuuuuuuhei <tak178.shu10@gmail.com>
2022-08-28 16:55:46 +0900

    サウナ登録用を追加

--- totonoe-app/src/@types/sauna/Sauna.ts
+++ totonoe-app/src/@types/sauna/Sauna.ts
@@ -1,10 +1,21 @@
 type Sauna = {
     id: string,
     facility_id: string,
-    sauna_type: string,
+    sauna_type: number,
     temperature: number,
     capacity: number,
-    humidity: string,
+    rouryu_kb: string,
+    bgm_kb: string,
+    tv_kb: string,
+    sauna_mat_kb: string,
+}
+
+type NewSauna = {
+    id: string,
+    facility_id: string,
+    sauna_type: number,
+    temperature: number,
+    capacity: number,
     rouryu_kb: string,
     bgm_kb: string,
     tv_kb: string,
