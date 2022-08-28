50d781ed9ebcd0ad70467efdafb3f723fb041ee1
shuuuuuuhei <tak178.shu10@gmail.com>
2022-08-28 16:52:41 +0900

    bootstrap.dropdownではなくinput.selectを使用するよう修正 住所情報のStateを更新できるよう修正

--- totonoe-app/src/components/form-components/SelectAdress.tsx
+++ totonoe-app/src/components/form-components/SelectAdress.tsx
@@ -1,13 +1,29 @@
-import React, { Component, Fragment, useState } from 'react'
-import { DropdownButton, ButtonGroup, Dropdown } from 'react-bootstrap'
+import React, { Component, Fragment, useState, ChangeEvent } from 'react'
+import { Form } from 'react-bootstrap'
 import { useAuth0 } from '@auth0/auth0-react';
 import { useCookies } from 'react-cookie';
+import { Input } from './Input';
+
+type selectAddressProps = {
+    address: {
+        prefectureID: number,
+        cityID: number,
+        street: string,
+
+    }
+    setAddress: React.Dispatch<React.SetStateAction<{
+        prefectureID: number;
+        cityID: number;
+        street: string;
+    }>>
+}
 
 type City = {
     id: number,
     name: string,
 }
-export const SelectAddress = () => {
+
+export const SelectAddress = (props: selectAddressProps) => {
     const {getAccessTokenSilently} = useAuth0();
     const [cookies, setCookie,removeCookie] = useCookies();
     
@@ -17,8 +33,9 @@ export const SelectAddress = () => {
     
     const prefectureList = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"];
 
-    const handlePrefecture = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        const index = parseInt(event.currentTarget.id);
+
+    const handlePrefecture = (event: ChangeEvent<HTMLSelectElement>) => {
+        const index = parseInt(event.target.value);
         setPrefectureIndex(index);
         setCityIndex(0);
         const prefectureID = index + 1;
@@ -40,7 +57,13 @@ export const SelectAddress = () => {
                         return response.json();
                     })
                     .then((data) => {
-                        setCityList(data)
+                        setCityList(data);
+
+                        // prefectureID(引数)の更新を行う
+                        props.setAddress((prevState) => ({
+                            ...prevState,
+                            prefectureID: prefectureID,
+                        }));
                     })
             }
             catch(err) {
@@ -50,51 +73,69 @@ export const SelectAddress = () => {
         fetchCityInfo();
     }
 
-    const handleCity = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        const index = parseInt(event.currentTarget.id);
-        setCityIndex(index)
+    const handleCity = (event: ChangeEvent<HTMLSelectElement>) => {
+        const index = parseInt(event.target.value);
+        console.log(index)
+        setCityIndex(index);
+        
+        if(!cityList) {
+            return
+        }
+
+        // 市町村ID（引数)の更新を行う
+        props.setAddress((prevState) => ({
+            ...prevState,
+            cityID: cityList[index].id,
+        }));
+    }
+
+    const handleStreetChange: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
+        const street = event.currentTarget.value;
+
+        // 住所詳細の更新を行う
+        props.setAddress((prevState) => ({
+            ...prevState,
+            street: street,
+        }))
     }
 
     return(
         <Fragment>
-            <div className="row py-3">
+            <div className="py-3">
                 <label htmlFor="">都道府県</label>
-                <div className="col-12">
-                    <DropdownButton
-                            as={ButtonGroup}
-                            id="dropdown-menu-align-responsive-1"
-                            title={prefectureList[prefectureIndex]}
-                            onChange={handlePrefecture}
-                        >
-                        <div className="row">    
-                            {prefectureList.map((prefecture, index) => {
-                                return(
-                                    <div className="col-3 px-3">
-                                        <Dropdown.Item as="button" onClick={handlePrefecture} id={index.toString()}>{prefecture}</Dropdown.Item>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </DropdownButton>
+                <div className="py-1">
+                    <Form.Select aria-label="Default select example" onChange={handlePrefecture} defaultValue="都道府県を選択">
+                        <option className="d-none" value="">都道府県を選択</option>
+                        {prefectureList.map((prefecture, index) => {
+                            return(
+                                <option value={index.toString()}>{prefecture}</option>
+                            )
+                        })}
+                    </Form.Select>
                 </div>
-                <label htmlFor="">市町村</label>
-                <div className="col-12">
-                    <DropdownButton
-                        as={ButtonGroup}
-                        id="dropdown-menu-align-responsive-1"
-                        title={!cityList ? <>都道府県を選択</> : cityList[cityIndex].name }
-                        onChange={handlePrefecture}
-                    >
-                        <div className="row">
-                            {cityList?.map((city, index) => {
-                                return(
-                                    <div className="col-3 px-3">
-                                        <Dropdown.Item as="button" id={index.toString()} onClick={handleCity}>{city.name}</Dropdown.Item>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </DropdownButton>
+                <label htmlFor="">市区町村</label>
+                <div className="py-1">
+                    <Form.Select aria-label="Default select example" onChange={handleCity}>
+                        <option className="d-none" value="">市町村を選択</option>
+                        {cityList?.map((city, index) => {
+                            return(
+                                <option value={index.toString()}>{city.name}</option>
+                            )
+                        })}
+                </Form.Select>
+                </div>
+                <label htmlFor="">町名番地</label>
+                <div className="py-1">
+                    <Input 
+                            type="text"
+                            className="input-sm"
+                            name="name"
+                            value={props.address?.street}
+                            onChange={handleStreetChange}
+                            placehodlder="町名番地を入力"
+                            errorDiv=""
+                            errorMsg=""
+                    />
                 </div>
             </div>
         </Fragment>
