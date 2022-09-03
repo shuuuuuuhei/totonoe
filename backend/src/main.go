2d9e3edb83767738b3d670775ba3eaaf9bcddd2a
shuuuuuuhei <tak178.shu10@gmail.com>
2022-09-03 22:48:30 +0900

    施設一覧取得機能を追加

--- backend/src/main.go
+++ backend/src/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"main.go/infra/database"
+	"main.go/infra/server"
 )
 
 func main() {
 	db := database.NewDB()
-	db.DBMigrate()
-	db.CreateData()
-	// r := server.Init(db)
-	// r.Run()
+	// db.DBMigrate()
+	// db.CreateData()
+	r := server.Init(db)
+	r.Run()
 }
--- backend/src/usecase/interactor/faclity.go
+++ backend/src/usecase/interactor/faclity.go
@@ -2,6 +2,7 @@ package interactor
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/model/ValueObject"
 	"main.go/usecase/port"
 )
 
@@ -10,12 +11,19 @@ type Facility struct {
 	FacilityRepo port.FacilityRepository
 }
 
-//
-func NewFacilityInputPort(o port.FacilityOutputPort, r port.FacilityRepository) port.FacilityInputPort {
-	return &Facility{
-		OutputPort:   o,
-		FacilityRepo: r,
+// GetFacilities implements port.FacilityInputPort
+func (*Facility) GetFacilities(*gin.Context) (*[]ValueObject.FacilityVO, error) {
+	panic("unimplemented")
+}
+
+// CreateFacility
+func (f *Facility) CreateFacility(c *gin.Context) {
+	err := f.FacilityRepo.CreateFacility(c)
+	if err != nil {
+		f.OutputPort.RenderError(err)
+		return
 	}
+	f.OutputPort.RenderOK()
 }
 
 func (f Facility) GetFacilityByID(c *gin.Context) {
@@ -28,5 +36,17 @@ func (f Facility) GetFacilityByID(c *gin.Context) {
 }
 
 func (f Facility) GetFacilitiesBySaunaOption(c *gin.Context) {
-	panic("not implemented") // TODO: Implement
+	facilities, err := f.FacilityRepo.GetFacilities(c)
+	if err != nil {
+		f.OutputPort.RenderError(err)
+		return
+	}
+	f.OutputPort.RenderFacilities(facilities)
+}
+
+func NewFacilityInputPort(o port.FacilityOutputPort, r port.FacilityRepository) port.FacilityInputPort {
+	return &Facility{
+		OutputPort:   o,
+		FacilityRepo: r,
+	}
 }
