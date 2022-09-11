88b7c9a167670a296924efc439735661e6a722a0
shuuuuuuhei <tak178.shu10@gmail.com>
2022-09-11 22:13:01 +0900

    緯度経度情報から取得したサウナ情報を表示するよう

--- totonoe-app/src/components/MapComponent.tsx
+++ totonoe-app/src/components/MapComponent.tsx
@@ -1,40 +1,34 @@
 import React, { Component, Fragment, useState } from 'react'
 import { GoogleMap, LoadScript, StandaloneSearchBox, Marker, InfoWindow } from "@react-google-maps/api";
 
-import * as dotenv from 'dotenv' 
-
+import "../style/Map.css"
 type Position = {
-    latlngLiteral: google.maps.LatLngLiteral,
-    placeName: string|undefined,
+    latlng_literal: google.maps.LatLngLiteral,
+    name: string|undefined,
 }
 type Libraries = ("drawing" | "geometry" | "localContext" | "places" | "visualization")[];
 
-const defaultLocation = { latlngLiteral: {lat: 35.69575,lng: 139.77521,}, placeName:""};
+const defaultLocation: Position = { name: "", latlng_literal: {lat: 35.69575,lng: 139.77521,},};
 const defaultZoom = 10;
-const infoWindowOptions = {
-    pixelOffset: new window.google.maps.Size(0, -45),
+const containerStyle = {
+    width: "auto",
+    height: "800px",
 };
 
+// const infoWindowOptions = {
+//     pixelOffset: new window.google.maps.Size(0, -45),
+// };
+
 export const MapComponent = () => {
-    const [placeState, setplaceState] = useState({
-        currentLocation: { lat: 0, lng: 0 },
-        markers: [],
-        bounds: null
-    });
+
     const [ libraries ] = useState<Libraries>(['places'])
     
     const [map, setMap] = useState<GoogleMap|null>();
     const [query, setQuery] = useState<google.maps.places.SearchBox>();
     const [locations, setLocation] = useState<Position[]>([defaultLocation]);
-    const [ facilityList, setFacilityList ] = useState<FacilityMapInfo>();
-
-    const containerStyle = {
-        width: "auto",
-        height: "800px",
-    };
     
     const onLoad = (ref: google.maps.places.SearchBox) => setQuery(ref);
-    
+    const [facilities, setFacilities] = useState<FacilityMapInfo[]>();
     const onPlacesChanged = () => {
         // 検索Boxから候補地を取得
         const places = query?.getPlaces();
@@ -46,82 +40,128 @@ export const MapComponent = () => {
 
             if(!lat || !lng) {return}
       
-            const position = {
-                latlngLiteral: {lat: lat(),lng: lng(),}, 
-                placeName:place.name,
+            const position: Position = { 
+                latlng_literal: {lat: lat(),lng: lng(),}, 
+                name: place.name,
             };
 
             newPositionList.push(position)
         })
+
+        console.log(newPositionList)
+
+        const fetchGetFacilitiesByMapInfo = async() => {
+            const uri = "http://localhost:4000/facilities/map_infomation";
+
+            const requestOption: RequestInit = {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    map_info_list: newPositionList,
+                })
+            };
+            await fetch(uri, requestOption)
+                .then((response) => {
+                    if (!response.ok) {
+                        const err = new Error;
+                        err.message = "施設情報の取得に失敗しました" + response.status;
+                        throw err;
+                    }
+                    return response.json();
+                })
+                .then((resData) => {
+                    setFacilities(resData)
+                })
+            .catch(err => {
+                console.log(err)
+            });
+        }
+
         setLocation(newPositionList)
-        
+        fetchGetFacilitiesByMapInfo();
     }
+    console.log(facilities)
 
     return(
         <Fragment>
-            <div className="row container">
-                <div className="col-4 facility-list">
-                    
+            <div className="container text-center">
+                <div className="row border-bottom">
+                    {facilities?.length}件表示
                 </div>
-                {process.env.REACT_APP_GOOGLE_MAP_API?
-                    <div google-map>
-                        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API} libraries={libraries} >
-                            <GoogleMap
-                                mapContainerStyle={containerStyle}
-                                center={locations[0].latlngLiteral}
-                                zoom={10}
-                                options={{
-                                    streetViewControl: false,
-                                    mapTypeControl: false,
-                                    fullscreenControl: false,
-                                }}
-                            >
-                                <div id="searchbox">
-                                    <StandaloneSearchBox
-                                        onLoad={onLoad}
-                                        onPlacesChanged={onPlacesChanged}
-                                    >
-                                    <input
-                                    type="text"
-                                    placeholder="Customized your placeholder"
-                                    style={{
-                                        boxSizing: `border-box`,
-                                        border: `1px solid transparent`,
-                                        width: `240px`,
-                                        height: `32px`,
-                                        padding: `0 12px`,
-                                        borderRadius: `3px`,
-                                        boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                                        fontSize: `14px`,
-                                        outline: `none`,
-                                        textOverflow: `ellipses`,
-                                        position: "absolute",
-                                        left: "50%",
-                                        marginLeft: "-120px"
-                                    }}
-                                    />
-                                    </StandaloneSearchBox>
+                <div className="row py-3">
+                    <div className="col-3 facility-list overflow-auto border py-4" style={containerStyle}>
+                        <h5 className="border-bottom">サウナ一覧</h5>
+                        {facilities?.map((facility, index) => {
+                            return(
+                                <div className="row text-start border-bottom py-2 search-facility">
+                                    <p>{facility.name}</p>
                                 </div>
-                                {locations.map((location, index) => {
-                                    return(
-                                        <>
-                                            <Marker position={location.latlngLiteral}/>
-                                            <InfoWindow position={location.latlngLiteral} options={infoWindowOptions}>
-                                            <div>
-                                                    <p>{location.placeName}</p>
-                                                    <p>サウナ情報あり</p>
-                                            </div>
-                                            </InfoWindow>
-                                        </>
-                                    )
-                                })}
-                                <br />
-                            </GoogleMap>
-                        </LoadScript>
+                            )
+                        })}
                     </div>
-                :
-                    <>マップ情報なし</>
-                }
+                    {process.env.REACT_APP_GOOGLE_MAP_API?
+                        <div className="google-map col-8">
+                            <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API} libraries={libraries} >
+                                <GoogleMap
+                                    mapContainerStyle={containerStyle}
+                                    center={locations[0].latlng_literal}
+                                    zoom={10}
+                                    options={{
+                                        streetViewControl: false,
+                                        mapTypeControl: false,
+                                        fullscreenControl: false,
+                                    }}
+                                >
+                                    <div id="searchbox">
+                                        <StandaloneSearchBox
+                                            onLoad={onLoad}
+                                            onPlacesChanged={onPlacesChanged}
+                                        >
+                                        <input
+                                        type="text"
+                                        placeholder="Customized your placeholder"
+                                        style={{
+                                            boxSizing: `border-box`,
+                                            border: `1px solid transparent`,
+                                            width: `240px`,
+                                            height: `32px`,
+                                            padding: `0 12px`,
+                                            borderRadius: `3px`,
+                                            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                                            fontSize: `14px`,
+                                            outline: `none`,
+                                            textOverflow: `ellipses`,
+                                            position: "absolute",
+                                            left: "50%",
+                                            marginLeft: "-120px"
+                                        }}
+                                        />
+                                        </StandaloneSearchBox>
+                                    </div>
+                                    {locations.map((location, index) => {
+                                        return(
+                                            <>
+                                                <Marker position={location.latlng_literal}/>
+                                                <InfoWindow position={location.latlng_literal}>
+                                                <div>
+                                                        <p>{location.name}</p>
+                                                        <p>サウナ情報あり</p>
+                                                </div>
+                                                </InfoWindow>
+                                            </>
+                                        )
+                                    })}
+                                    <br />
+                                </GoogleMap>
+                            </LoadScript>
+                        </div>
+                    :
+                        <>マップ情報なし</>
+                    }
+                </div>
             </div>
         </Fragment>
     )
