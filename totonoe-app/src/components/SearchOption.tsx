a933c4663192310967c10a26a3707320f6e04466
shuuuuuuhei <tak178.shu10@gmail.com>
2022-09-17 14:21:07 +0900

    再検索用のサウナオプションを

--- totonoe-app/src/components/SearchOption.tsx
+++ totonoe-app/src/components/SearchOption.tsx
@@ -1,41 +1,54 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment, useState } from 'react'
 import { Input } from './form-components/Input'
-import { Button } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
 import { InputScope } from './form-components/InputScope'
+import { Link } from 'react-router-dom'
+
+const termsList = [
+    "宿泊", "飯処", "コワーキングスペース", "外気浴", "読書", "給水機", "熱波師",
+]
 
 export const SearchOption = () => {
-    const handleChange = () => {
 
+    const [temperature, setTemperature] = useState({start: "", end: ""});
+    const [price, setPrice] = useState({start: "", end: ""})
+
+    const handleTemperatureChange = (event: React.ChangeEvent<HTMLInputElement>) => {    
+        // 入力区分(開始or終了)を受け取る
+        const inputKb = event.target.id;
+        setTemperature({...temperature, [inputKb]: event.target.value});
     }
-
-    const termsList = [
-        "宿泊", "飯処", "コワーキングスペース", "外気浴", "読書", "給水機", "熱波師",
-    ]
-
+    
+    const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        // 入力区分(開始or終了)を受け取る
+        const inputKb = event.target.id;
+        setPrice({...temperature, [inputKb]: event.target.value});
+    }
+    
     return(
         <Fragment>
             <div className="option-box border px-3 py-2 text-start">
                 <label htmlFor="" className="py-3">■条件を絞る</label>
-                <div className="temperature-option row">
+                <Form.Group className="temperature row py-2">
+                    <Form.Label>温度</Form.Label>
                     <InputScope
                         className="input-sm"
                         name=""
-                        value=""
-                        onChange={handleChange}
+                        valueStart={temperature.start.toString()}
+                        valueEnd={temperature.end.toString()}
+                        onChange={handleTemperatureChange}
                         placehodlder="温度"
-                        errorDiv=""
-                        errorMsg=""
                     />
-                </div>
-                <div className="temperature-option row">
+                </Form.Group>
+                <div className="temperature-option row py-2">
+                    <Form.Label>値段</Form.Label>
                     <InputScope
                         className="input-sm"
                         name=""
-                        value=""
-                        onChange={handleChange}
+                        valueStart={price.start}
+                        valueEnd={price.end}
+                        onChange={handlePriceChange}
                         placehodlder="値段"
-                        errorDiv=""
-                        errorMsg=""
                     />
                 </div>
                 <div className="terms-options text-start">
@@ -52,7 +65,7 @@ export const SearchOption = () => {
                 </div>
                 <div className="row">
                     <div className="research-button py-2 col-6">
-                        <Button>検索</Button>
+                        <Link to="/search"><Button className="btn-warning btn-blockb　w-auto">検索</Button></Link>
                     </div>
                     <div className="clear-button py-2 col-6">
                         <Button>クリア</Button>
