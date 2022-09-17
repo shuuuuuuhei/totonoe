bb538eaa174213b59b12186399c1cc36cf76369f
shuuuuuuhei <tak178.shu10@gmail.com>
2022-09-17 14:19:02 +0900

    閲覧履歴用に枠を追加

--- totonoe-app/src/pages/HomePage.tsx
+++ totonoe-app/src/pages/HomePage.tsx
@@ -2,10 +2,21 @@ import React, { Fragment } from "react"
 import Alert from 'react-bootstrap/Alert';
 import { ArticlesBox } from "../components/ArticlesBox";
 import { SearchBox } from "../components/SearchBox";
+import { AreaBox } from "../components/AreaBox";
 export const Home: React.VFC = () => {
     return (
         <Fragment>
             <SearchBox />
+            <div className="container text-center">
+                <div className="row my-5">
+                    <div className="col-10">
+                        <AreaBox />
+                    </div>
+                    <div className="col-2">
+                        閲覧履歴
+                    </div>
+                </div>
+            </div>
             <ArticlesBox />
         </Fragment>
     );
