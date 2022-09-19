51ad27f198f72b3fd811c2141f2c346fe6731624
shuuuuuuhei <tak178.shu10@gmail.com>
2022-09-19 21:00:03 +0900

    空文字変換にnulllの対応を追加

--- totonoe-app/src/common/Convert.ts
+++ totonoe-app/src/common/Convert.ts
@@ -8,7 +8,7 @@ export const UndefinedConvertToZero = (val: number|undefined) => {
 /**
  * 値を受け取り、Undefinedなら空文字に変換する
  */
-export const UndefinedConvertToEmpty = (val: string|undefined) => {
+export const UndefinedOrNullConvertToEmpty = (val: string|undefined|null) => {
     return !val ? "" : val
 }
 
