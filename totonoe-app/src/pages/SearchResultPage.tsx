762f2a90891df55e6cce3b21d043924d24dcf1f3
shuuuuuuhei <tak178.shu10@gmail.com>
2022-09-19 20:56:18 +0900

    ・都道府県を選択していない場合は都道府県を一覧から選択することで条件に都道府県を追加できるよう修正 ・都道府県に所属する市区町村を選択した場合は条件に市区町村を追加できるように修正 ※市区町村まで選択済みの場合はエリアの条件追加は行わない

--- totonoe-app/src/pages/SearchResultPage.tsx
+++ totonoe-app/src/pages/SearchResultPage.tsx
@@ -6,11 +6,18 @@ import { ModalHover } from 'react-modal-hover'
 import { HiOutlinePencilAlt } from 'react-icons/hi';
 import { BsHeart } from 'react-icons/bs';
 import { FacilityList } from '../components/SaunaList';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useCookies } from 'react-cookie';
 import { SearchOption } from '../components/SearchOption';
 import { Link, useLocation } from 'react-router-dom';
 import { UndefinedOrNullConvertToEmpty } from '../common/Convert';
+import { prefectureList } from '../utils/constants';
+
+const baseUri = 'http://localhost:4000/facilities?';
+
+type city = {
+    id: string,
+    name: string,
+    prefectureID: string,
+}
 
 export const SearchResultPage = () => {
     const { search } = useLocation();
@@ -21,6 +28,8 @@ export const SearchResultPage = () => {
     const priceEnd = UndefinedOrNullConvertToEmpty(queryParams.get("price_end"));
     const temperatureStart = UndefinedOrNullConvertToEmpty(queryParams.get("temperature_start"));
     const temperatureEnd = UndefinedOrNullConvertToEmpty(queryParams.get("temperature_end"));
+    const [detailAreaPrefectureList, setDetailArea] = useState<string[]>();
+    const [detailAreaCityList, setDetailAreaCityList] = useState<city[]>([]);
 
     const [selected, setSelected] = useState({
         key: "",
@@ -36,8 +45,9 @@ export const SearchResultPage = () => {
     const [facilities, setFacilitiesState] = useState<Facility[]>();
     const [show, setShow] = useState(false);
 
+
     const handleSearch = (searchOption: string) => {
-        const uri = `http://localhost:4000/facilities?area=${areaParams}&facilityName=${facilityName}${searchOption}`;
+        const uri = baseUri+`area=${areaParams}&facilityName=${facilityName}${searchOption}`;
 
         const requestOption: RequestInit = {
             method: "GET",
@@ -66,7 +76,7 @@ export const SearchResultPage = () => {
             });
         }
         fetchSaunas();
-    }
+       }
 
     useEffect(() => {
         const fetchSaunas = async() => {
@@ -97,7 +107,45 @@ export const SearchResultPage = () => {
             });
         }
         fetchSaunas();
-    }, [])
+
+        // 全国の場合は都道府県リストを参照するため、都道府県リストをエリアリストに格納して処理を終了
+        if(areaParams === '') {
+            setDetailArea(prefectureList)
+            return
+        }
+
+        // 取得した都道府県から市区町村を取得する
+        const fetchAreaDetail = async () => {
+            // 「県」を省いた検索になるので前方一致で検索をかける
+            const prefectureID = prefectureList.findIndex(prefecture => prefecture.startsWith(areaParams)) + 1;
+            const uri = `http://localhost:4000/prefecture/${prefectureID}/cities`;
+            const requestOption: RequestInit = {
+                method: "GET",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+            await fetch(uri, requestOption)
+                .then((response) => {
+                    if (!response.ok) {
+                        const err = new Error;
+                        err.message = "市区町村取得に失敗しました" + response.status;
+                        throw err;
+                    }
+                    return response.json();
+                })
+                .then((resData) => {
+                    setDetailAreaCityList(resData);
+                })
+            .catch(err => {
+                console.log(err)
+            });
+        }
+
+        fetchAreaDetail();
+
+    }, [areaParams])
 
     if(!facilities) {
         return(
@@ -106,7 +154,45 @@ export const SearchResultPage = () => {
             </Fragment>
         )
     }
-    
+
+    // エリアフィールドのホバー時に表示するエリア詳細を設定する
+    const AreaDetail = () => {
+
+        // 都道府県+市区町村を検索済みの場合は表示しない
+        if(areaParams && detailAreaCityList.length === 0) {
+            return(
+                null
+            )
+        }
+
+        return(
+            <Fragment>
+                <div className="border row container" style={{position: "absolute", top: "450px", left: "400px", backgroundColor: "white", width: "850px",}}>
+                    {areaParams === '' ?
+                    // 全国の都道府県を表示
+                        detailAreaPrefectureList?.map((detailArea) => {
+                            return(
+                                <div className="col-1 py-2 text-center">
+                                    <Link to={`/search?lang=jp&area=${detailArea}`}><p className="m-0 py-1" style={{fontSize: "10px",fontWeight: 'bold'}}>{detailArea}</p></Link>
+                                </div>
+                            )
+                        })
+                    :
+                    //　都道府県に所属する市区町村を表示
+                        detailAreaCityList.map((detailAreaCity) => {
+                            return(
+                                <div className="col-1 py-2 text-center">
+                                    <Link to={`/search?lang=jp&area=${areaParams}${detailAreaCity.name}`}><p className="m-0 py-1" style={{fontSize: "10px",fontWeight: 'bold'}}>{detailAreaCity.name}</p></Link>
+                                </div>
+                            )
+                        })
+                    } 
+                </div>
+            </Fragment>
+        )
+    }
+
+    //{show && <p style={{ color: 'red', fontWeight: 'bold' }}>{prefectureList.map((prefecture) => prefecture)}</p>}
 
     return(
         <Fragment>
@@ -116,8 +202,8 @@ export const SearchResultPage = () => {
                         <div className="row text-start border p-3 mb-3">
                             <label htmlFor="" className="py-3">■エリアを絞る</label>
                             <div className="area" onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
-                                <p className="border-bottom" style={{cursor: 'pointer'}}>{areaParams === '' ? "全国" : areaParams}</p>
-                                {show && <p style={{ color: 'red', fontWeight: 'bold' }}>Tooltipに表示させたい内容をここに記述します。</p>}
+                                <p className="border-bottom" style={{cursor: 'pointer', fontWeight: 'bold'}}>{areaParams === '' ? "全国" : areaParams}</p>
+                                {show && <AreaDetail />}
                             </div>
                             <button><Link to='/map'>GoogleMapで探す</Link></button>
                         </div>
