994ccc93452037b400595192c9ab69e71de5ff5c
shuuuuuuhei <tak178.shu10@gmail.com>
2022-09-25 22:49:22 +0900

    ・タイトルの表示を削除 ・user情報とLike、Comment情報をヘッダにして、評価と内容をメインに表示するように修正

--- totonoe-app/src/components/Article.tsx
+++ totonoe-app/src/components/Article.tsx
@@ -99,39 +99,35 @@ export const DetailArticle: React.VFC<ArticleProps> = (props) => {
 
     return(
         <Fragment>
-            <div className="article-wrap">
-                <div className="article-header row justify-content-center">
-                    <div className="col-10 article-title">
-                        <h3><Link to={'/saunas/'+article?.facility_id+'/articles/'+article?.id}>{article?.title}</Link></h3>
-                    </div>
-                    <div className="col-2 article-top-right">
-                        <div className="row justify-content-center">
-                            <div className="col-2 article-like-count">
-                                <GrLike size={30} onClick={handleLike}/>
-                                <p>{article?.like_count}</p>
-                            </div>
-                            <div className="col-2 article-comment-count">
-                                <FaRegCommentDots size={30}/>
-                                <p>{article?.comment_count}</p>
+                <div className="article-wrap p-3 border container text-center my-3 p-5" style={{width: "800px"}}>
+                    <Link
+                        to={`/articles/${article?.id}`}
+                        style={{
+                            textDecoration: "none",
+                        }}
+                    >
+                    <div className="row">
+                        <div className="col-9">
+                            <div className="row text-center">
+                                <div className="col-3 user-icon text-end py-2">
+                                    <MdInsertEmoticon size={30}/>
+                                </div>
+                                <div className="col-9 user text-start">
+                                    <Link to={`/profile/${article?.user_id}`}><p className="user-name m-0">{article?.user_name}</p></Link>
+                                    <p className="article-date m-0">{setDateFormat(article?.created_at)}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className="article-contents">
-                    <div className="article-top row">
-                        <div className="col-1 user-icon">
-                            <MdInsertEmoticon size={30}/>
-                        </div>
-                        <div className="col-3 user">
-                            <p className="user-name">{article?.user_name}</p>
-                            <p className="article-date">{setDateFormat(article?.CreatedAt)}</p>
-                        </div>
-                        <div className="col-8 sauna text-center">
-                            <div className="sauna-name">
-                            <h3>{article?.facility_name}</h3>
-                            </div>
-                            <div className="sauna-place">
-                                {article?.facility_name}
+                        <div className="col-3 article-right">
+                            <div className="row text-end">
+                                <div className="col-6 article-like-count">
+                                    <GrLike size={30} onClick={handleLike}/>
+                                    <p>{article?.like_count}</p>
+                                </div>
+                                <div className="col-6 article-comment-count">
+                                    <FaRegCommentDots size={30}/>
+                                    <p>{article?.comment_count}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -140,10 +136,13 @@ export const DetailArticle: React.VFC<ArticleProps> = (props) => {
                             評価
                         </div>
                         <div className="col-7 article-content">
+                            <div className="facility text-start">
+                                <Link to={`/saunas/${article?.facility_id}`}><p className="m-0">{article?.facility_name}</p></Link>
+                            </div>
                             <p>{article?.content}</p>
                         </div>
                     </div>
-                </div>
+                </Link>
             </div>
         </Fragment>
     )
