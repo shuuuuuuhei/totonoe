ec1929dcb8c27a89b4fd1d04d501a457ade9950a
shuuuuuuhei <tak178.shu10@gmail.com>
2022-09-25 22:49:41 +0900

    ・nullまたはundefinedまたは空文字の判定を行う処理を追加

--- totonoe-app/src/common/Check.ts
+++ totonoe-app/src/common/Check.ts
@@ -1,7 +1,7 @@
 /**
- * nullまたはundefinedの判定を行う
+ * nullまたはundefinedまたは空文字の判定を行う
  */
-export const IsNullOrUndefined = (val: any) => {
-    if(val === null || !val) return true;
+export const IsNullOrUndefinedOrEmpty = (val: any) => {
+    if(val === null || !val || val === "") return true;
     return false;
 }
