590d53b0420b433e66066fa211c49bf147c05ac2
shuuuuuuhei <tak178.shu10@gmail.com>
2022-10-01 22:46:44 +0900

    記事モデルに評価項目を追加

--- backend/src/model/Domain/article.go
+++ backend/src/model/Domain/article.go
@@ -2,17 +2,23 @@ package Domain
 
 import (
 	"time"
+
+	"github.com/shopspring/decimal"
 )
 
 // Article 記事モデル
 type Article struct {
-	ID           uint32 `gorm:"primaryKey" json:"id"`
-	FacilityID   uint32 `gorm:"not null" json:"sauna_id"`
-	UserID       string `gorm:"not null" json:"user_id"`
-	Title        string `gorm:"not null" json:"title"`
-	Content      string `gorm:"not null" json:"content"`
-	ArticleLikes []ArticleLike
-	Comments     []Comment
-	CreatedAt    time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt    time.Time `gorm:"autoUpdateTime;datetime(0)"`
+	ID            uint32              `gorm:"primaryKey" json:"id"`
+	FacilityID    uint32              `gorm:"not null" json:"sauna_id"`
+	UserID        string              `gorm:"not null" json:"user_id"`
+	Content       string              `gorm:"not null" json:"content"`
+	TotonoiScore  decimal.NullDecimal `gorm:"type:decimal" json:"totonoi_score,omitempty"`
+	RelaxScore    decimal.NullDecimal `gorm:"type:decimal" json:"relax_score,omitempty"`
+	PriceScore    decimal.NullDecimal `gorm:"type:decimal" json:"price_score,omitempty"`
+	ServiceScore  decimal.NullDecimal `gorm:"type:decimal" json:"service_score,omitempty"`
+	AmbienceScore decimal.NullDecimal `gorm:"type:decimal" json:"ambience_score,omitempty"`
+	ArticleLikes  []ArticleLike
+	Comments      []Comment
+	CreatedAt     time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt     time.Time `gorm:"autoUpdateTime;datetime(0)"`
 }
