f4222bc8e793725a4f8f8dc4deb7a84c13e8ad7b
shuuuuuuhei <tak178.shu10@gmail.com>
2022-10-01 22:42:26 +0900

    施設個別画面に評価機能は表示しない

--- totonoe-app/src/pages/FacilityPage.tsx
+++ totonoe-app/src/pages/FacilityPage.tsx
@@ -96,7 +96,6 @@ export const SaunaPage = () => {
                 <ul className="nav nav-pills nav-fill py-3">
                     <li className="nav-item"><button id='nav-1' className="nav-link active" onClick={handleClick}>施設情報</button></li>
                     <li className="nav-item"><button id='nav-2' className="nav-link" onClick={handleClick}>投稿</button></li>
-                    <li className="nav-item"><button id='nav-3' className="nav-link" onClick={handleClick}>評価</button></li>
                 </ul>
                 <div className="sauna-contents">
                     {
