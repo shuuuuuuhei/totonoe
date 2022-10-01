5ec0ec6010f4666c31c48e7fb6c21452624567b8
shuuuuuuhei <tak178.shu10@gmail.com>
2022-10-01 22:43:56 +0900

    評価用フォーム作成

--- totonoe-app/src/pages/ArticlePostPage.tsx
+++ totonoe-app/src/pages/ArticlePostPage.tsx
@@ -1,15 +1,22 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import React, { ChangeEventHandler, Component, Fragment, useState } from 'react'
-import { Alert, Form, Button } from 'react-bootstrap'
+import React, { Component, Fragment, useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
 import { Link, useParams, useNavigate } from 'react-router-dom'
 import { Article } from '../@types/article/Article'
-import { Input } from '../components/form-components/Input'
-import { Textarea } from '../components/form-components/Textarea'
+
 import { NewArticle } from '../@types/article/NewArticle'
 import { useCookies } from "react-cookie";
 import { IsNullOrUndefinedOrEmpty } from '../common/Check'
 import { UndefinedConvertToZero, UndefinedOrNullConvertToEmpty } from '../common/Convert'
 import { toast } from 'react-toastify'
+import DatePicker, { registerLocale } from "react-datepicker";
+import ja from 'date-fns/locale/ja';
+import "react-datepicker/dist/react-datepicker.css";
+import { RatingScore, RatingOptionProps } from '../@types/article/Rating'
+import { defaultScore, precisionScore, ratingList } from 'utils/constants'
+import { Rating } from '@mui/material';
+import Box from '@mui/material/Box';
+import StarIcon from '@mui/icons-material/Star';
 
 type Data = {
     article: Article,
@@ -18,6 +25,23 @@ type Data = {
 
 const baseUri = "http://localhost:4000/"
 
+
+const labels: { [index: string]: string } = {
+    0.5: 'Useless',
+    1: 'Useless+',
+    1.5: 'Poor',
+    2: 'Poor+',
+    2.5: 'Ok',
+    3: 'Ok+',
+    3.5: 'Good',
+    4: 'Good+',
+    4.5: 'Excellent',
+    5: 'Excellent+',
+};
+
+const getLabelText = (value: number) => {
+    return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+}
 
 /**
  * サウナ施設 IDをリンクから受け取って施設情報と記事情報を紐づける処理を行う(紐付け処理は未着手)
@@ -71,8 +95,16 @@ export const ArticlePostPage = () => {
         UserID: "",
         SaunaID: "",
     });
+
     const {getAccessTokenSilently} = useAuth0();
     const [cookies, setCookie,removeCookie] = useCookies();
+    const [rating, setRatingScore] = useState<RatingScore>({
+        totonoi_score: defaultScore,
+        relax_score: defaultScore,
+        price_score: defaultScore,
+        ambience_score: defaultScore,
+        service_score: defaultScore,
+    });
     
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const name = event.target.name;
@@ -106,7 +138,7 @@ export const ArticlePostPage = () => {
                     Authorization: `Bearer ${accessToken}`,
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({article, 'user_id': cookies.userID, "facility_id": parseInt(UndefinedOrNullConvertToEmpty(facilityID)),})
+                body: JSON.stringify({article, 'user_id': cookies.userID, "facility_id": parseInt(UndefinedOrNullConvertToEmpty(facilityID)), "rating": rating})
             }
             fetch(uri, requestOption)
                 .then((response) => response.json())
@@ -119,47 +151,89 @@ export const ArticlePostPage = () => {
             console.log("エラー",error)
         }
     }
+    const [startDate, setStartDate] = useState(new Date());
+    registerLocale('ja', ja);
+
+    const RatingForm: React.VFC<RatingOptionProps> = (ratingOptionProps) => {
+
+        const handleScore = (newScore: number | null) => {
+            setRatingScore({
+                ...rating, [ratingOptionProps.id]: newScore,
+            })
+            setRatingScore({
+                ...rating, [ratingOptionProps.id]: newScore,
+            })
+        }
+
+        return(
+            <>
+                <Rating
+                    className="col-1"
+                    value={rating[ratingOptionProps.id]}
+                    defaultValue={defaultScore}
+                    precision={precisionScore}
+                    getLabelText={getLabelText}
+                    onChange={(event, newValue) => {handleScore(newValue)}}
+                    emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                />
+                {rating[ratingOptionProps.id] !== null && (
+                    <Box className="col-2 py-1" sx={{ ml: 2 }}>{rating[ratingOptionProps.id]}</Box>
+                )}
+        </>
+        )
+    }
+
+    console.log(rating)
 
     return (
         <Fragment>
-                <h2>ととのい日記を投稿する</h2>
-
-                <hr />
-                <div className="facility">
-                    <label htmlFor="">施設名：</label>
-                    <h3>{facilityName}</h3>
+            <div className="container p-5 text-center">
+                <div className="facility py-4">
+                    <h1>{facilityName}</h1>
+                </div>
+                <div className="row visited-date text-start py-4">
+                    <div className="col-1">訪問日</div>
+                    <div className="col-1">
+                        <DatePicker 
+                            selected={startDate}
+                            onChange={(date:Date) => setStartDate(date)}
+                            dateFormat="yyyy/MM/dd"
+                            locale='ja'
+                        />
+                    </div>
                 </div>
                 <Form onSubmit={handleSubmit}>
-                    <Form.Group className="form-title">
-                        <Form.Label htmlFor="">タイトル</Form.Label>
-                        <Form.Control 
-                                type="text"
-                                className="input-sm"
-                                name="Title"
-                                value={article.Title}
-                                onChange={handleChange}
-                                placeholder="タイトルを入力"
-                                required={true}
-                        />
-                    </Form.Group>
                     <Form.Group className="form-content">
-                        <Form.Label htmlFor="">内容</Form.Label>
                         <Form.Control 
-                                type="textarea"
+                                as="textarea"
                                 className="input-sm"
                                 name="Content"
                                 value={article.Content}
                                 onChange={handleChange}
-                                placeholder="内容を入力"
+                                placeholder="ととのいを"
                                 required={true}
+                                rows={10}
                         />
                     </Form.Group>
+                    <div className="text-start">
+                        {ratingList.map((rating, index) => {
+                            return(
+                                <div className="row py-3">
+                                    <div className="col-2">
+                                        <p>{rating.name}</p>
+                                    </div>
+                                    <RatingForm id={rating.id}/>
+                                </div>
+                            )
+                        })}
+                    </div>
                     <hr />
-                    <Button type="submit" className="btn btn-primary">Save</Button>
+                    <Button type="submit" className="btn btn-primary">投稿</Button>
                     <Link to="/" className="btn btn-warning ms-1">
                         Cancel
                     </Link>
                 </Form>
+            </div>
         </Fragment>
     )
 }
