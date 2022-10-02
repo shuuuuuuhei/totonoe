d6d2121af303a82a36d6986a4b54dcbf56bf05ef
shuuuuuuhei <tak178.shu10@gmail.com>
2022-10-02 11:39:11 +0900

    表示用の記事構造体に評価項目を追加

--- backend/src/model/ValueObject/Article.go
+++ backend/src/model/ValueObject/Article.go
@@ -2,18 +2,25 @@ package ValueObject
 
 import (
 	"time"
+
+	"github.com/shopspring/decimal"
 )
 
 type ArticleVO struct {
-	ID           uint32    `json:"id,omitempty"`
-	Content      string    `json:"content,omitempty"`
-	CreatedAt    time.Time `json:"created_at,omitempty"`
-	UpdatedAt    time.Time `json:"updated_at,omitempty"`
-	FacilityID   uint32    `json:"facility_id,omitempty"`
-	FacilityName string    `json:"facility_name,omitempty"`
-	UserID       string    `json:"user_id,omitempty"`
-	UserName     string    `json:"user_name,omitempty"`
-	LikeCount    int8      `json:"like_count,omitempty"`
-	IsLiked      bool      `json:"is_liked,omitempty"`
-	CommentCount int8      `json:"comment_count,omitempty"`
+	ID            uint32              `json:"id,omitempty"`
+	Content       string              `json:"content,omitempty"`
+	CreatedAt     time.Time           `json:"created_at,omitempty"`
+	UpdatedAt     time.Time           `json:"updated_at,omitempty"`
+	FacilityID    uint32              `json:"facility_id,omitempty"`
+	FacilityName  string              `json:"facility_name,omitempty"`
+	UserID        string              `json:"user_id,omitempty"`
+	UserName      string              `json:"user_name,omitempty"`
+	LikeCount     int8                `json:"like_count,omitempty"`
+	IsLiked       bool                `json:"is_liked,omitempty"`
+	CommentCount  int8                `json:"comment_count,omitempty"`
+	TotonoiScore  decimal.NullDecimal `gorm:"type:decimal" json:"totonoi_score,omitempty"`
+	RelaxScore    decimal.NullDecimal `gorm:"type:decimal" json:"relax_score,omitempty"`
+	PriceScore    decimal.NullDecimal `gorm:"type:decimal" json:"price_score,omitempty"`
+	ServiceScore  decimal.NullDecimal `gorm:"type:decimal" json:"service_score,omitempty"`
+	AmbienceScore decimal.NullDecimal `gorm:"type:decimal" json:"ambience_score,omitempty"`
 }
