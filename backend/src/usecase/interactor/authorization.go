b647cca0f8b93339fca177f06b9e299111434f5d
shuuuuuuhei <tak178.shu10@gmail.com>
2022-10-15 22:34:16 +0900

    認可機能のoutportを作成

---
+++ backend/src/usecase/interactor/authorization.go
@@ -0,0 +1,38 @@
+package interactor
+
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/usecase/port"
+)
+
+type Authorization struct {
+	Outputport port.AuthorizationOutputPort
+	Repository port.AuthorizationRepository
+}
+
+// ApplySubmitFacilityAuth implements port.AuthorizationInputPort
+func (a *Authorization) ApplySubmitFacilityAuth(c *gin.Context) {
+	err := a.Repository.ApplySubmitFacilityAuth(c)
+	if err != nil {
+		a.Outputport.RenderError(err)
+		return
+	}
+	a.Outputport.RenderOK()
+}
+
+// CertificationAuth implements port.AuthorizationInputPort
+func (a *Authorization) CertificationAuth(c *gin.Context) {
+	err := a.Repository.CertificationAuth(c)
+	if err != nil {
+		a.Outputport.RenderError(err)
+		return
+	}
+	a.Outputport.RenderOK()
+}
+
+func NewAuthorizationInputPort(o port.AuthorizationOutputPort, repo port.AuthorizationRepository) port.AuthorizationInputPort {
+	return &Authorization{
+		Outputport: o,
+		Repository: repo,
+	}
+}
