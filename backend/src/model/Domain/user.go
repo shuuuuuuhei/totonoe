262ebd90720816812bfd515c22fde98019fa7820
shuuuuuuhei <tak178.shu10@gmail.com>
2022-10-15 22:31:49 +0900

    profileを削除

--- backend/src/model/Domain/user.go
+++ backend/src/model/Domain/user.go
@@ -6,13 +6,14 @@ import (
 
 // User ユーザモデル
 type User struct {
-	ID           string `gorm:"type:varchar(100);primaryKey"`
-	Name         string `gorm:"not null"`
-	Email        string `gorm:"not null"`
-	Articles     []Article
-	Profile      Profile
-	ArticleLikes []ArticleLike
-	Following    []*User   `gorm:"many2many:user_relation_ship"`
-	CreatedAt    time.Time `gorm:"autoCreateTime"`
-	UpdatedAt    time.Time `gorm:"autoUpdateTime"`
+	ID            string `gorm:"type:varchar(100);primaryKey"`
+	Name          string `gorm:"not null"`
+	Email         string `gorm:"not null"`
+	Introduction  string `gorm:"type:varchar(100);"`
+	Articles      []Article
+	ArticleLikes  []ArticleLike
+	Following     []*User `gorm:"many2many:user_relation_ship"`
+	Authorization Authorization
+	CreatedAt     time.Time `gorm:"autoCreateTime"`
+	UpdatedAt     time.Time `gorm:"autoUpdateTime"`
 }
