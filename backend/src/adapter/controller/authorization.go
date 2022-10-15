8eec0edde9f80247116c8e783579b9086af961c0
shuuuuuuhei <tak178.shu10@gmail.com>
2022-10-15 22:36:11 +0900

    以下のinputportを作成 ・ ApplySubmitFacilityAuth 施設登録権限の申請を登録する ・ CertificationAuth 施設登録権限申請承認処理

---
+++ backend/src/adapter/controller/authorization.go
@@ -0,0 +1,31 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"main.go/usecase/port"
+)
+
+type Authorization struct {
+	InputPortFactory  func(o port.AuthorizationOutputPort, r port.AuthorizationRepository) port.AuthorizationInputPort
+	OutputPortFactory func(c *gin.Context) port.AuthorizationOutputPort
+	RepositoryFactory func(conn *gorm.DB) port.AuthorizationRepository
+	Conn              *gorm.DB
+}
+
+func (a *Authorization) ApplySubmitFacilityAuth(c *gin.Context) {
+	inputPort := a.newInputPort(c)
+	inputPort.ApplySubmitFacilityAuth(c)
+}
+
+func (a *Authorization) CertificationAuth(c *gin.Context) {
+	inputPort := a.newInputPort(c)
+	inputPort.CertificationAuth(c)
+}
+
+func (a Authorization) newInputPort(c *gin.Context) port.AuthorizationInputPort {
+	output := a.OutputPortFactory(c)
+	repository := a.RepositoryFactory(a.Conn)
+	inputport := a.InputPortFactory(output, repository)
+	return inputport
+}
