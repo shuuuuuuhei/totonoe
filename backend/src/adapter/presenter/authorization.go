060810f56c04b350e4d8dd33fd2cb23dc97abc3d
shuuuuuuhei <tak178.shu10@gmail.com>
2022-10-31 20:38:24 +0900

    権限情報Render処理

--- backend/src/adapter/presenter/authorization.go
+++ backend/src/adapter/presenter/authorization.go
@@ -2,11 +2,17 @@ package presenter
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/model/Domain"
 	"main.go/usecase/port"
 )
 
 type Authorization struct {
 	c *gin.Context
+}
+
+// RenderAuthorization implements port.AuthorizationOutputPort
+func (a *Authorization) RenderAuthorization(authorization *Domain.Authorization) {
+	a.c.JSON(200, authorization)
 }
 
 // RenderError implements port.AuthorizationOutputPort
