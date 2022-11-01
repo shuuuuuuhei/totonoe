789d72e2c757d2230a8aef186fc1fefb244cdf56
shuuuuuuhei <tak178.shu10@gmail.com>
2022-11-01 23:15:59 +0900

    権限情報取得処理を追加

--- backend/src/usecase/interactor/authorization.go
+++ backend/src/usecase/interactor/authorization.go
@@ -10,7 +10,19 @@ type Authorization struct {
 	Repository port.AuthorizationRepository
 }
 
-// GetAuthorization implements port.AuthorizationInputPort
+// GetApplyingAuthorization implements port.AuthorizationInputPort
+func (a *Authorization) GetApplyingAuthorization(c *gin.Context) {
+	applyingAuthorizationList, err := a.Repository.GetApplyingAuthorization(c)
+
+	if err != nil {
+		a.Outputport.RenderError(err)
+		return
+	}
+
+	a.Outputport.RenderApplyingUserList(applyingAuthorizationList)
+}
+
+// GetAuthorization 権限情報取得
 func (a *Authorization) GetAuthorization(c *gin.Context) {
 	authorization, err := a.Repository.GetAuthorization(c)
 
@@ -18,6 +30,14 @@ func (a *Authorization) GetAuthorization(c *gin.Context) {
 		a.Outputport.RenderError(err)
 		return
 	}
+
+	// 権限情報がない場合は値を設定せず200成功レスポンスを行う
+	if authorization == nil {
+		a.Outputport.RenderOK()
+		return
+	}
+
+	// 権限情報を返す
 	a.Outputport.RenderAuthorization(authorization)
 }
 
