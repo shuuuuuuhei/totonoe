231e4c787fa6ab41202ba28ac399ae9158922c48
shuuuuuuhei <tak178.shu10@gmail.com>
2022-11-06 22:16:24 +0900

    ・都道府県名からインデックスを取得する処理を追加 ・保存による整形

--- totonoe-app/src/common/Convert.ts
+++ totonoe-app/src/common/Convert.ts
@@ -1,31 +1,32 @@
+import { prefectureList } from '../utils/constants';
 /**
  * 値を受け取り、Undefinedなら0に変換する
  */
-export const UndefinedConvertToZero = (val: number|undefined) => {
+export const UndefinedConvertToZero = (val: number | undefined) => {
     return !val ? 0 : val
 }
 
 /**
  * 値を受け取り、Undefinedなら空文字に変換する
  */
-export const UndefinedOrNullConvertToEmpty = (val: string|undefined|null) => {
+export const UndefinedOrNullConvertToEmpty = (val: string | undefined | null) => {
     return !val ? "" : val
 }
 
 /**
  * 文字列を日付フォーマットに変換する
  */
-export const StrConvertStrTime = (start: string|undefined, end: string|undefined) => {
-    if(!start || !end) {
+export const StrConvertStrTime = (start: string | undefined, end: string | undefined) => {
+    if (!start || !end) {
         return
     }
 
     // 翌日まで営業しているパターン(例 08:00 ~ (翌)2:00)
-    if(parseInt(start.split(":")[0]) >= parseInt(end.split(":")[0])) {
-        return(start + "~(翌)"+ end);
+    if (parseInt(start.split(":")[0]) >= parseInt(end.split(":")[0])) {
+        return (start + "~(翌)" + end);
     }
 
-    return(start + "~" + end);
+    return (start + "~" + end);
 }
 
 /**
@@ -34,7 +35,7 @@ export const StrConvertStrTime = (start: string|undefined, end: string|undefined
  * 1以外→"-"
  */
 export const ConvertKBToMaruORHyphen = (kb: string) => {
-    if(kb != "1") return "-"
+    if (kb != "1") return "-"
     return "○"
 }
 
@@ -42,6 +43,14 @@ export const ConvertKBToMaruORHyphen = (kb: string) => {
  * NaN(非数値)であれば１に変換する
 */
 export const ConvertNaNToOne = (num: number) => {
-    if(isNaN(num)) return 1
+    if (isNaN(num)) return 1
     return num
+}
+
+/**
+ * 都道府県名からインデックスを取得
+ * @param prefectureName 
+ */
+export const ConvertPrefectureNameToIndex = (prefectureName: string) => {
+    return prefectureList.indexOf(prefectureName)
 }
