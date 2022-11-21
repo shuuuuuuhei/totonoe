4f291c8d5e61d321d57844655403ec5a1bc51e2e
shuuuuuuhei <tak178.shu10@gmail.com>
2022-11-21 23:38:52 +0900

    条件検索を表示するボタンを追加

--- totonoe-app/src/components/SearchBox.tsx
+++ totonoe-app/src/components/SearchBox.tsx
@@ -1,12 +1,26 @@
 import React, { Fragment, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Modal, Typography, Box } from '@mui/material';
+import { SelectFilterBox } from './form-components/SelectFilterBox';
 
 const MinPageCount = 1;
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 800,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 export const SearchBox = () => {
 
     const [area, setArea] = useState("");
     const [facilityName, setFacilityName] = useState("");
+    const [isModalShowed, setIsModalShowed] = useState(false);
 
     const handleArea = (event: React.ChangeEvent<HTMLInputElement>) => {
         setArea(event.target.value)
@@ -41,9 +55,25 @@ export const SearchBox = () => {
                         />
                     </div>
                     <div className="col-1 p-0">
-                        <Button>
+                        <Button onClick={() => { setIsModalShowed(true) }}>
                             特徴を指定
-                            </Button>
+                        </Button>
+                        <Modal
+                            open={isModalShowed}
+                            onClose={() => { setIsModalShowed(!isModalShowed) }}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box sx={style}>
+                                <Typography className="text-center" variant="h6" component="h2">
+                                    特徴を選択する
+                                </Typography>
+                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                    {/* 条件選択ボックス */}
+                                    <SelectFilterBox />
+                                </Typography>
+                            </Box>
+                        </Modal>
                     </div>
                     <div className="col-1">
                         <Link to={`/search?lang=jp&area=${area}&name=${facilityName}&page=${MinPageCount}`}><Button className="btn-warning btn-blockb　w-auto">検索</Button></Link>
