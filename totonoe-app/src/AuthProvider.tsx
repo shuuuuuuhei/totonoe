d0db6cc2b38489361aa36f2ac11b5b60f41828b5
shuuuuuuhei <tak178.shu10@gmail.com>
2023-03-06 21:52:23 +0900

    auth0バージョンアップによる修正

--- totonoe-app/src/AuthProvider.tsx
+++ totonoe-app/src/AuthProvider.tsx
@@ -18,7 +18,6 @@ export const Auth0ProviderWithHistory = ({ children }: {
         <Auth0Provider
             domain={process.env.REACT_APP_AUTH0_DOMAIN}
             clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-            redirectUri={window.location.origin}
             onRedirectCallback={onRedirectCallback}
         >
             {children}
